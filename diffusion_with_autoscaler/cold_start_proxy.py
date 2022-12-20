b2a5cb71aa6de4a430a1bef1ded8dd0e8ef5d882
Sherin Thomas <sherin@lightning.ai>
2022-12-20 14:34:20 +0000

    getting-from-master

--- diffusion_with_autoscaler/cold_start_proxy.py
+++ diffusion_with_autoscaler/cold_start_proxy.py
@@ -1,6 +1,8 @@
+import asyncio
 from typing import Any
 
 import aiohttp
+from fastapi import HTTPException
 from pydantic import BaseModel
 
 from diffusion_with_autoscaler.datatypes import Image, Text
@@ -9,12 +11,49 @@ proxy_url = "https://ulhcn-01gd3c9epmk5xj2y9a9jrrvgt8.litng-ai-03.litng.ai/api/p
 
 
 class ColdStartProxy:
+    """ColdStartProxy allows users to configure the load balancer to use a proxy service while the work is cold
+    starting. This is useful with services that gets realtime requests but startup time for workers is high.
+
+    If the request body is same and the method is POST for the proxy service,
+    then the default implementation of `handle_request` can be used. In that case
+    initialize the proxy with the proxy url. Otherwise, the user can override the `handle_request`
+
+    Args:
+        proxy_url (str): The url of the proxy service
+    """
+
     def __init__(self, proxy_url):
         self.proxy_url = proxy_url
-        self.proxy_timeout = 20
+        self.proxy_timeout = 50
+        # checking `asyncio.iscoroutinefunction` instead of `inspect.iscoroutinefunction`
+        # because AsyncMock in the tests requres the former to pass
+        if not asyncio.iscoroutinefunction(self.handle_request):
+            raise TypeError("handle_request must be an `async` function")
 
     async def handle_request(self, request: BaseModel) -> Any:
-        pass
+        """This method is called when the request is received while the work is cold starting. The default
+        implementation of this method is to forward the request body to the proxy service with POST method but the
+        user can override this method to handle the request in any way.
+
+        Args:
+            request (BaseModel): The request body, a pydantic model that is being
+            forwarded by load balancer which is a FastAPI service
+        """
+        try:
+            async with aiohttp.ClientSession() as session:
+                headers = {
+                    "accept": "application/json",
+                    "Content-Type": "application/json",
+                }
+                async with session.post(
+                        self.proxy_url,
+                        json=request.dict(),
+                        timeout=self.proxy_timeout,
+                        headers=headers,
+                ) as response:
+                    return await response.json()
+        except Exception as ex:
+            raise HTTPException(status_code=500, detail=f"Error in proxy: {ex}")
 
 
 class CustomColdStartProxy(ColdStartProxy):
