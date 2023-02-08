3019e5604c864bcb4bf3b2bf254c57ecdddea8b6
Akihiro Nitta <aki@lightning.ai>
2023-02-08 16:50:53 +0000

    update toy

--- app_interruptible_toy.py
+++ app_interruptible_toy.py
@@ -1,59 +1,32 @@
 # !pip install 'git+https://github.com/Lightning-AI/LAI-API-Access-UI-Component.git'
-# !curl https://raw.githubusercontent.com/Lightning-AI/stablediffusion/lit/configs/stable-diffusion/v1-inference.yaml -o v1-inference.yaml
 import lightning as L
-import os, base64, io, torch
+import time
 from diffusion_with_autoscaler import AutoScaler, BatchText, BatchImage, Text, Image, IntervalReplacement
-
-PROXY_URL = "https://ulhcn-01gd3c9epmk5xj2y9a9jrrvgt8.litng-ai-03.litng.ai/api/predict"
-
-class FlashAttentionBuildConfig(L.BuildConfig):
-
-    def build_commands(self):
-        return ["pip install 'git+https://github.com/Lightning-AI/stablediffusion.git@lit'"]
-
 
 class DiffusionServer(L.app.components.PythonServer):
     def __init__(self, *args, **kwargs):
         super().__init__(
             input_type=BatchText,
             output_type=BatchImage,
-            cloud_build_config=FlashAttentionBuildConfig(),
             *args,
             **kwargs,
         )
 
     def setup(self):
-        import ldm
-        if not os.path.exists("v1-5-pruned-emaonly.ckpt"):
-            cmd = "curl -C - https://pl-public-data.s3.amazonaws.com/dream_stable_diffusion/v1-5-pruned-emaonly.ckpt -o v1-5-pruned-emaonly.ckpt"
-            os.system(cmd)
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        self._model = ldm.lightning.LightningStableDiffusion(
-            config_path="v1-inference.yaml",
-            checkpoint_path="v1-5-pruned-emaonly.ckpt",
-            device=device,
-            deepspeed=True, # Supported on Ampere and RTX, skipped otherwise.
-            context="no_grad",
-            flash_attention="triton",
-            steps=30,
-        )
+        print(f"{time.time()}: {self.__class__}.setup()")
 
     def predict(self, requests):
+        print(f"{time.time()}: {self.__class__}.predict()")
         texts = [request.text for request in requests.inputs]
-        images = self._model.predict_step(prompts=texts, batch_idx=0)
-        results = []
-        for image in images:
-            buffer = io.BytesIO()
-            image.save(buffer, format="PNG")
-            image_str = base64.b64encode(buffer.getvalue()).decode("utf-8")
-            results.append(image_str)
+        results = texts
+        time.sleep(6 * len(requests))
         return BatchImage(outputs=[{"image": image_str} for image_str in results])
 
 
 component = AutoScaler(
     DiffusionServer,  # The component to scale
-    cloud_compute=L.CloudCompute("gpu-rtx", interruptible=True, disk_size=80),
-    strategy=IntervalReplacement(interval=30 * 60),
+    cloud_compute=L.CloudCompute("gpu", interruptible=True),
+    strategy=IntervalReplacement(interval=8),  # in seconds
 
     # autoscaler args
     min_replicas=1,
@@ -61,7 +34,7 @@ component = AutoScaler(
     endpoint="/predict",
     scale_out_interval=0,
     scale_in_interval=600,
-    max_batch_size=6,
+    max_batch_size=1,
     timeout_batching=0.3,
     input_type=Text,
     output_type=Image,
